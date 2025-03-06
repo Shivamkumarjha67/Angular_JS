0e6eb960e06f0b281137a773c74a294920fb2b2d
Shivam Kumar Jha <shkrjh@gmail.com>
2025-03-06 11:58:55 +0530

    [Shivam Kumar Jha] Add . Event handling, redirecting to the website of bridgelabz when clicked on logo.

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -9,9 +9,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Hello World';
-  imgUrl = '../asset/BL_logo_square_jpg.jpg';
+  imgUrl = 'https://imgs.search.brave.com/TKWOH1gzjT880iA72cMl2zTUFjhnIDwoSys4kjasSME/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMueW91cnN0b3J5/LmNvbS9jcy9pbWFn/ZXMvY29tcGFuaWVz/L2xvZ28tMTU4NjQx/OTU3NDMzNy5qcGc_/Zm09YXV0byZhcj0x/OjEmbW9kZT1maWxs/JmZpbGw9c29saWQm/ZmlsbC1jb2xvcj1m/ZmYmZm9ybWF0PWF1/dG8mdz0zODQmcT03/NQ';
+  url = 'https://www.bridgelabz.com/'
 
   ngOnInit() : void {
     this.title = "Hello from BridgeLabz!";
+  }
+
+  onClick($event : any) {
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
   }
 }
