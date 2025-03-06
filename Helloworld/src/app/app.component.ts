57ae9ee3befd529703c8909f65355029f0a731b5
Shivam Kumar Jha <shkrjh@gmail.com>
2025-03-06 11:49:28 +0530

    [Shivam Kumar Jha] Add . Displaying the bridgelabz logo using property binding technique.

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -9,8 +9,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Hello World';
-  imgUrl = '../asset/BL_logo_square_jpg.jpg';
-
+  imgUrl = "https://imgs.search.brave.com/TKWOH1gzjT880iA72cMl2zTUFjhnIDwoSys4kjasSME/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMueW91cnN0b3J5/LmNvbS9jcy9pbWFn/ZXMvY29tcGFuaWVz/L2xvZ28tMTU4NjQx/OTU3NDMzNy5qcGc_/Zm09YXV0byZhcj0x/OjEmbW9kZT1maWxs/JmZpbGw9c29saWQm/ZmlsbC1jb2xvcj1m/ZmYmZm9ybWF0PWF1/dG8mdz0zODQmcT03/NQ"
   ngOnInit() : void {
     this.title = "Hello from BridgeLabz!";
   }
