c5e4d741f4aa7ac45a37974db8c633f0ab036e5b
Shivam Kumar Jha <shkrjh@gmail.com>
2025-03-06 13:04:38 +0530

    [Shivam Kumar Jha] Add . Displaying the validated inputted user name

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
   imgUrl = 'https://imgs.search.brave.com/TKWOH1gzjT880iA72cMl2zTUFjhnIDwoSys4kjasSME/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMueW91cnN0b3J5/LmNvbS9jcy9pbWFn/ZXMvY29tcGFuaWVz/L2xvZ28tMTU4NjQx/OTU3NDMzNy5qcGc_/Zm09YXV0byZhcj0x/OjEmbW9kZT1maWxs/JmZpbGw9c29saWQm/ZmlsbC1jb2xvcj1m/ZmYmZm9ybWF0PWF1/dG8mdz0zODQmcT03/NQ';
   url = 'https://www.bridgelabz.com/';
   userName: string = "";
+  nameError : String = "";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz!";
@@ -22,5 +23,17 @@ export class AppComponent {
   onClick($event: any) {
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event : any) {
+    console.log("Change Event occurred!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\a]{2,}$');
+
+    if(nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+
+    this.nameError = "Name is incorrect!";
   }
 }
